30678bb73bedcf3896b76cd7b68786e59a4efb38
Birsu Ion <ion.birsu@softwire.com>
2020-05-06 11:13:10 +0300

    lab 10 t

---
+++ min.ts
@@ -0,0 +1,14 @@
+import *  as _ from "lodash";
+
+type Node = number;
+const Inf = 99999999999999;
+
+const V: Node[] = [1, 2, 3, 4, 5, 6, 7];
+const E = [
+    [1, 5, 10],
+    [1, 3, 50],
+    [1, 4, 30],
+    [2, 3, 20],
+    [3, 5, 10],
+    [4, 5, 20],
+];
